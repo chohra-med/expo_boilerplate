1bdc59e0b2ee0615e7a3547a9ec2834e5b490e88
chohra-med <choumed02@gmail.com>
2025-11-13 14:48:09 +0100

    add env to gitignore, update jest config and clean app rules

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,7 @@ module.exports = {
     '<rootDir>/coverage/',
   ],
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|react-native-uuid)',
   ],
   moduleNameMapper: {
     '^#root/(.*)$': '<rootDir>/src/$1',
